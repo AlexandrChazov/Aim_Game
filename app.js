4a475ea3cd998538b1959c5f5794e634a9d0538a
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2021-06-18 23:23:17 +0300

    refactor: improve code

--- app.js
+++ app.js
@@ -8,34 +8,49 @@ const colors = ["#000000", "#ffffff", "#f70a1a", "#f70ae0", "#900af7", "#1e0af7"
 let time = 0;
 let score = 0;
 
-startButton.addEventListener("click", () => {
+startButton.addEventListener("click", (event) => {
+  event.preventDefault();
   screens[0].classList.add("up");
 })
 
 timeButtons.addEventListener("click", (event) => {
   if (event.target.classList.contains('time-btn')) {
-    time = parseInt(event.target.attributes[1].nodeValue);
-    startGame();
+    startGame(event);
+    screens[1].classList.add("up");
+  }
+})
+
+function startGame(event) {
+  time = parseInt(event.target.attributes[1].nodeValue);
     const interval = setInterval(() => {
       setTime();
     }, 1000);
     setTimeout(() => {
       clearInterval(interval);
     }, ((time+1)*1000));
-    screens[1].classList.add("up");
-  }
-})
+  addCircle();
+}
 
-function setTime() {
-  if (time === 0) {
-    finishGame();
-  }
-  if (time < 10) {
-    timeSpan.innerHTML = `00:0${time}`
-  } else {
-    timeSpan.innerHTML = `00:${time}`
-  }
-  time--;
+function addCircle() {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  const diameter = getRandomNumber(7,25);
+  circle.style.width = circle.style.height = `${diameter}px`;
+  const {width, height} = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width-diameter);
+  const y = getRandomNumber(0, height-diameter);
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  const color = colors[randomIndex];
+  circle.style.background = color;
+  circle.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+  circle.addEventListener("click", () => {
+    score++;
+    circle.remove();
+    addCircle()
+  })
+  board.append(circle);
 }
 
 function finishGame() {
@@ -55,33 +70,14 @@ function finishGame() {
   }, 3000);
 }
 
-function startGame() {
-  addCircle();
-}
-
-function setPosition(min, max) {
+function getRandomNumber(min, max) {
   return Math.round(Math.random()*(max-min)+min)
 }
 
-function addCircle() {
-  const circle = document.createElement("div");
-  circle.classList.add("circle");
-  const diameter = setPosition(7,25);
-  circle.style.width = `${diameter}px`;
-  circle.style.height = `${diameter}px`;
-  const {width, height} = board.getBoundingClientRect();
-  const x = setPosition(0, width-diameter);
-  const y = setPosition(0, height-diameter);
-  circle.style.left = `${x}px`;
-  circle.style.top = `${y}px`;
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  const color = colors[randomIndex];
-  circle.style.background = color;
-  circle.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
-  circle.addEventListener("click", () => {
-    score++;
-    circle.remove();
-    addCircle()
-  })
-  board.append(circle);
+function setTime() {
+  time === 0 && finishGame();
+  time < 10
+    ? timeSpan.innerHTML = `00:0${time}`
+    : timeSpan.innerHTML = `00:${time}`;
+  time--;
 }
